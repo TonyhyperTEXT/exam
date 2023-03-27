c55c66e2b0213d16f1c65e66f94cc7d4604e7a35
Anton <119679174+TonyhyperTEXT@users.noreply.github.com>
2023-03-27 13:12:09 +0400

    Add files via upload

---
+++ .access.php
@@ -0,0 +1,12 @@
+<?
+$PERM["ask"]["7"]="X";
+$PERM["rew"]["6"]="X";
+$PERM["search"]["5"]="W";
+$PERM["products"]["5"]="W";
+$PERM["services"]["5"]="W";
+$PERM["news"]["5"]="W";
+$PERM["contacts"]["5"]="W";
+$PERM["company"]["5"]="W";
+$PERM["index.php"]["5"]="W";
+$PERM["/"]["*"]="R";
+?>
